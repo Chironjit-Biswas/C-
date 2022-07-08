cedcb28a41a5e29c2d21ee956ff5f6c04fad4742
Chironjit Biswas <78298949+Chironjit-Biswas@users.noreply.github.com>
2022-07-08 13:14:03 +0600

    Add files via upload

---
+++ Two Number Division.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    int num1,num2,div;
+
+    cout<<"Enter 1st Number : ";
+    cin>>num1; // User Input
+
+    cout<<"Enter 2nd Number : ";
+    cin>>num2; // User Input
+
+    div=num1/num2;
+
+
+    cout<<"Division Number : "<<div;
+
+
+    getch();
+    return 0;
+}
