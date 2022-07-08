9a0e17c0d322d3a52763a88eb338751c379fe6d8
Chironjit Biswas <78298949+Chironjit-Biswas@users.noreply.github.com>
2022-07-08 13:07:11 +0600

    Add files via upload

---
+++ Two Number Subtract.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    int num1,num2,sub;
+
+    cout<<"Enter 1st Number : ";
+    cin>>num1; // User Input
+
+    cout<<"Enter 2nd Number : ";
+    cin>>num2; // User Input
+
+    sub=num1-num2;
+
+
+    cout<<"Subtract Number : "<<sub;
+
+
+    getch();
+    return 0;
+}
