5dbd35be5ed8599bf59570dbb21763b640e7b8e4
Chironjit Biswas <78298949+Chironjit-Biswas@users.noreply.github.com>
2022-07-08 12:59:00 +0600

    Add files via upload

---
+++ Two Number Sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    int num1,num2,sum;
+
+    cout<<"Enter 1st Number : ";
+    cin>>num1; // User Input
+
+    cout<<"Enter 2nd Number : ";
+    cin>>num2; // User Input
+
+    sum=num1+num2;
+
+    cout<<"Total Number : "<<sum;
+
+
+    getch();
+    return 0;
+}
